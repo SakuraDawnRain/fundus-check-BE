6e75c17d2097c330886db288c9b8745c75518127
Andy <2280546952@qq.com>
2021-04-29 01:38:17 +0800

    完成flask后端渲染html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,render_template
 from flask_cors import cross_origin
 import os,random,string,base64
 import util
@@ -9,24 +9,26 @@ app = Flask(__name__)
 @app.route('/')
 @cross_origin()
 def hello():
-    return 'Hello, this IMED from SUSTech'
+    return render_template('up.html')
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    img = request.files['file']
+    # 获取POST来的图片
+    img = request.files['photo']
+    # 产生随机图片名 存储到/temp_save/ 生成热力图的对应url
     ran_str = ''.join(random.sample(string.ascii_letters + string.digits, 16))
-    path = basedir+"/temp_save/"
-    imgName = ran_str+img.filename
-    file_path = path+imgName
-    img.save(file_path)
-    classification,heatmap_url=util.temp_func(file_path)
-    classification=util.class_transf(classification)
-    with open(heatmap_url, 'rb') as f:
+    imgName = ran_str+'_'+img.filename
+    path_save = basedir+"/temp_save/"
+    path_heatmap = basedir+"/temp_heatmap"
+    file_path_save = path_save+imgName
+    file_path_heatmap = path_heatmap+imgName
+    img.save(file_path_save)
+    # 调取ZM的API 传图片url 获取类别int 获取类名和热力图字节流转字符流
+    classification=util.temp_func(file_path_save,file_path_heatmap)
+    with open(file_path_save, 'rb') as f:
             heatmap_base64 = base64.b64encode(f.read())
     heatmap_base64=heatmap_base64.decode('utf-8')
-    result = {
-        'class_name': classification,
-        'heatmap': heatmap_base64
-    }
-    return jsonify(result)
+    # 打包进json 发回前端
+    
+    return render_template("test.html",n=classification,u=heatmap_base64)
--- util.py
+++ util.py
@@ -1,14 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def temp_func(user_url):
+def temp_func(user_url,heatmap_url):
     #to-do
-    classification = 1
-    heatmap_url=basedir+"/temp_heatmap/"+"imed.png"
-    return classification,heatmap_url
-
-def class_transf(class_int):
-    classification="?"
-    if(class_int==1):
-        classification="Cataract"
+    classification = "Cataract"
     return classification
